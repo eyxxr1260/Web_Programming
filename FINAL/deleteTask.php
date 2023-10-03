5310be4f5c3eef8e8d776076396fe48c16c14e78
陳卿雅 <93075590+eyxxr1260@users.noreply.github.com>
2023-10-03 11:44:05 +0800

    Update deleteTask.php

--- FINAL/deleteTask.php
+++ FINAL/deleteTask.php
@@ -1,6 +1,6 @@
 <?php
     $key=$_POST["key"];
-    $db = mysqli_connect( "localhost" , "root", "pupu1123yaya","track"  );
+    $db = mysqli_connect( "localhost" , "root", "password","track"  );
     mysqli_query($db,"DELETE FROM `track` WHERE `ID`='$key'");
     mysqli_close( $db );
 ?>
