b5e4d2fd112557b7df147e9ef8ea76a5c04ecbf7
陳卿雅 <93075590+eyxxr1260@users.noreply.github.com>
2023-10-03 11:43:51 +0800

    Update addTask.php

--- FINAL/addTask.php
+++ FINAL/addTask.php
@@ -3,7 +3,7 @@ $USER = $_POST["User"];
 $DATE = $_POST["date"];
 $FootPrint = $_POST["FootPrint"];
 $Cost = $_POST["Cost"];
-$DB = mysqli_connect("localhost", "root", "pupu1123yaya","track");
+$DB = mysqli_connect("localhost", "root", "password","track");
 //mysqli_query($DB, "SELECT * FROM `track`");
 mysqli_query($DB, "INSERT INTO track(`FootPrint`, `Cost`, `Date`, `UserName`) VALUES ('$FootPrint','$Cost','$DATE','$USER')");
 mysqli_close($DB);
