bec8da6a728c47db6e1647f691e675e72f2c84f9
陳卿雅 <93075590+eyxxr1260@users.noreply.github.com>
2023-10-03 11:44:36 +0800

    Update editTask.php

--- FINAL/editTask.php
+++ FINAL/editTask.php
@@ -3,7 +3,7 @@
     $changeFootPrint=$_POST["changeFootPrint"];
     $changeCost=$_POST["changeCost"];
     $changeDate=$_POST["changeDate"];
-    $db = mysqli_connect( "localhost","root", "pupu1123yaya","track" );
+    $db = mysqli_connect( "localhost","root", "password","track" );
     mysqli_query($db,"UPDATE `track` SET `FootPrint`='$changeFootPrint',`Cost`='$changeCost',`Date`='$changeDate' WHERE `ID`='$key'");
     mysqli_close( $db );
 ?>
